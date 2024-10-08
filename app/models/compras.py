bbda92891c1c9baa2a2231188f41260f5b5b6c1e
danaguzman22 <dannaguman@gmail.com>
2024-10-08 11:25:32 -0300

    últimos arreglos

--- app/models/compras.py
+++ app/models/compras.py
@@ -4,7 +4,7 @@ from app import db
 class Compras(db.Model):
     __tablename__ = "compras"
     id = db.Column(db.Integer, primary_key=True)
-    producto_id = db.Column(db.Integer, db.ForeignKey("productos.id"))
+    producto_id = db.Column(db.Integer)
     fecha_compra = db.Column(db.DateTime)
     direccion_envio = db.Column(db.String(255))
 
